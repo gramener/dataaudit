a60ff6bc519c52b5bcba50ff1ce627acf0cc2f47
naveen.manukonda <naveen.manukonda@gramener.com>
2018-07-18 17:39:04 +0530

    ENH: Added code for logging the print statements.

--- test_data.py
+++ test_data.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import dataaudit
+import logging
 # from dataaudit import load, check
 
 folder = os.path.dirname(os.path.abspath(__file__))
@@ -21,12 +22,16 @@ def test_missing_values():
 
 def test_mutiple_columns():
     filename = os.path.join(repo, 'sales.csv')
+    log_file = os.path.join(repo, 'log.log')
     header_row, data = dataaudit.load_data(filename, 'csv')
-    print(dataaudit.duplicate_columns_name(header_row))
-    print(dataaudit.duplicate_columns_untyped(data))
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    logging.basicConfig(filename=log_file, filemode='w')
+    logging.info(dataaudit.duplicate_columns_name(header_row))
+    logging.info(dataaudit.duplicate_columns_untyped(data))
     filename = os.path.join(repo, 'sales.xlsx')
     header_row, data = dataaudit.load_data(filename, 'excel')
-    print(dataaudit.duplicate_columns_name(header_row))
-    print(dataaudit.duplicate_columns_untyped(data))
+    logging.info(dataaudit.duplicate_columns_name(header_row))
+    logging.info(dataaudit.duplicate_columns_untyped(data))
 
 test_mutiple_columns()

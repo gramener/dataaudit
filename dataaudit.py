7efe09c2dfad0def9cb5f20855d3ebb037f78ccb
skathirmani <kathir.sukumar@gmail.com>
2018-08-07 16:42:13 +0530

    WIP: restructuring calling functions

--- dataaudit.py
+++ dataaudit.py
@@ -2,7 +2,17 @@
 import six
 import sys
 import itertools
+import utils
 import pandas as pd
+
+
+def is_a_file(source, meta={}):
+    errors = {}
+    # First need to check if it is a file
+    header, data = utils.read_csv(source)
+    meta['header'] = header
+    meta['types'] = utils.types(data)
+    return data, meta, errors
 
 
 def check(source, **kwargs):
@@ -10,10 +20,11 @@ def check(source, **kwargs):
     Returns a list of data quality errors
     '''
     errors = []
+    meta = {}
     # Load the data
     if isinstance(source, six.text_type):
         if is_a_file(source):
-            data, errors = check_file(source)
+            data, meta, error = is_a_file(source, meta)
         elif is_a_database(source):
             data = pd.read_sql(source, **kwargs)
     elif isinstance(source, pd.DataFrame):
@@ -23,145 +34,27 @@ def check(source, **kwargs):
 
     # Apply type checks and conversations
     for method in registry['data-untyped']:
-        report(errors, method(data))
+        report(errors, method(data, meta), meta)
     for method in registry['column-untyped']:
         for col in data.columns:
-            report(errors, method(data[col]))
+            report(errors, method(data[col], meta), meta)
 
     # Check typed data
     for method in registry['data-typed']:
-        report(errors, method(data))
+        report(errors, method(data, meta), meta)
     for method in registry['column-typed']:
         for col in data.columns:
-            report(errors, method(data[col]), column=col)
+            report(errors, method(data[col], meta), meta)
 
     return errors
 
 
-def report(errors, result, **kwargs):
+def report(errors, result, meta, **kwargs):
     if result is None:
         return
     result.update(kwargs)
     errors.append(result)
-
-
-def load(path_or_file, **kwargs):
-    '''
-    Returns a dictionary with 2 keys:
-
-    - ``data``: a Pandas DataFrame with the contents of the file. ``None`` if
-      the file could not be loaded
-    - ``error``: a list of file format or column types data errors
-    '''
-    pass
-
-
-def missing_values_untyped(series, max=0, values=['', 'NA']):
-    '''
-    Reports number of missing values in a series
-    '''
-    strings = series[series.apply(lambda v: isinstance(v, six.string_types))]
-    values = set(values)
-    na = strings.apply(lambda v: v in values).sum()
-    null = pd.isnull(series).sum()
-    missing = null + na
-    if missing > max:
-        return {
-            'code': 'missing_value_untyped',
-            'message': '{}: {:.0f} values missing'.format(series.name, missing),
-            'series': series.name,
-            'missing': missing,
-            'na': na,
-            'null': null,
-        }
-
-
-def duplicate_rows_untyped(data):
-    count_duplicates = data.duplicated().sum()
-    if count_duplicates > 0:
-        return {
-            'code': 'duplicate_rows_untyped',
-            'message': '{:.0f} duplicate rows'.format(count_duplicates),
-            'duplicates': count_duplicates,
-        }
-
-
-def check_numeric(series):
-    '''Given a string series, checks if the value is numeric or not.
-    - If fully numeric, no errors
-    - If it's supposed to be numeric (based on heuristics) but is not, report an error
-    - If it's clearly not numeric, no errors
-    '''
-    pass
-
-
-def count_numeric_outliers(series, low=None, high=None, max=0):
-    '''Given a numerical series, counts number of outliers
-    - If low is not specified, 2 percentile is taken
-    - If high is not specified, 98 percentile is taken
-
-    '''
-    if series._get_numeric_data().shape[0] == 0:
-        return None
-    q1 = series.quantile(0.25)
-    q3 = series.quantile(0.75)
-    iqr = q3 - q1
-    if low is None:
-        low = q1 - 1.5 * iqr
-    if high is None:
-        high = q3 + 1.5 * iqr
-    lower_outliers = series[series < low].shape[0]
-    upper_outliers = series[series > high].shape[0]
-    outliers = lower_outliers + upper_outliers
-    if outliers > max:
-        return {
-            'code': 'count_outliers_typed',
-            'message': 'Outliers count in {} column(numeric): \
-            {:.0f}'.format(series.name, outliers),
-            'series': series.name,
-            'outliers': outliers,
-            'lower_outliers': lower_outliers,
-            'upper_outliers': upper_outliers,
-            'low': low,
-            'high': high,
-        }
-
-
-def nulls_patterns(data):
-    '''
-    '''
-    nulls_pattern = {}
-    missing_cols = data.columns[pd.isnull(data).sum() > 0]
-    for comb in range(len(missing_cols), 0, -1):
-        for i in itertools.combinations(missing_cols, comb):
-            cols = list(i)
-            non_nulls = data[cols].dropna(how='all')
-            if(non_nulls.shape[0] < data.shape[0]):
-                nulls_pattern[i] = data.shape[0] - non_nulls.shape[0]
-                data = data.loc[non_nulls.index]
-    if len(nulls_pattern) > 1:
-        return {
-                'code': 'missing-patterns',
-                'message': 'missing patterns found',
-                'md_pattern': nulls_pattern
-        }
-
-
-def count_categorical_outliers(series):
-    # Need to handle long tail
-    if len(series._get_numeric_data()) == 1:
-        return None
-    series_freq = series.value_counts()
-    steepest_slope = series_freq[series_freq.diff() / series_freq.shift(1) < -0.5]
-    if len(steepest_slope):
-        outliers = len(series_freq[series_freq <= steepest_slope.values[0]])
-        message = 'Outliers count in %s column(categorical): %d' % (series.name, outliers)
-        return {
-            'code': 'count_categorical_outliers_typed',
-            'series': series.name,
-            'outliers': outliers,
-            'message': message
-        }
+    return errors
 
 
 registry = {
@@ -173,49 +66,23 @@ registry = {
     'data-typed': [],
     'column-typed': [],
 }
-registry['column-untyped'].append(missing_values_untyped)
-registry['data-untyped'].append(duplicate_rows_untyped)
+
+
+registry['column-untyped'].append(utils.missing_values_untyped)
+registry['data-untyped'].append(utils.duplicate_rows_untyped)
+'''
 registry['column-typed'].append(count_numeric_outliers)
 registry['column-typed'].append(count_categorical_outliers)
 registry['data-untyped'].append(nulls_patterns)
-
-
-def print_test(test_output):
-    if test_output is not None:
-        print(test_output['message'])
-
-
-def run_audits(path):
-    # Check this function with Anand
-    # How should we print results
-    # Hence not optimizing
-    try:
-        print(path)
-        data = pd.read_csv(path)
-        print('========Data Untyped==============')
-        for method in registry['data-untyped']:
-            test_data = method(data)
-            print_test(test_data)
-        print('========Column Untyped============')
-        for method in registry['column-untyped']:
-            for col in data.columns:
-                col_test = method(data[col])
-                print_test(col_test)
-        print('=======Column Typed=================')
-        for method in registry['column-typed']:
-            for col in data.columns:
-                col_test = method(data[col])
-                print_test(col_test)
-    except (FileNotFoundError, pd.errors.ParserError) as e:
-        print(e)
-        print('Problem loading the data. Pass the correct CSV file wit correct path')
-    return 'Audits sucessfully done'
+'''
 
 
 if __name__ == "__main__":
     args = sys.argv
     if len(args) > 1:
         path = sys.argv[1]
-        run_audits(path)
+        errors = check(path)
+        for error in errors:
+            print(error['code'], ' | ', error['message'])
     else:
         print('No file path passed. Pass one')
--- utils.py
+++ utils.py
@@ -1,6 +1,13 @@
+import os
+import six
+import csv
+import xlrd
+import sys
+import itertools
 import dateutil
 import datetime
 import pandas as pd
+import numpy as np
 
 def is_date(series):
     '''
@@ -62,7 +69,7 @@ def has_keywords(series, sep=' ', thresh=2):
 
 def get_numeric_cols(data):
     numeric_data = data._get_numeric_data()
-    return numeric_data.columns
+    return list(numeric_data.columns)
 
 
 def types(data):
@@ -96,3 +103,146 @@ def types(data):
     typ['dates'] = [col for col in data.columns if is_date(data[col])]
     typ['keywords'] = [col for col in typ['groups'] if has_keywords(data[col])]
     return typ
+
+
+def read_csv(filepath):
+    '''
+    Read given CSV file.
+    '''
+    delimiter = ","
+    with open(filepath) as csvfile:
+        dialect = csv.Sniffer().sniff(csvfile.read(1024))
+        csvfile.seek(0)
+        delimiter = dialect.delimiter
+        header_row = next(csvfile).strip('\n').split(delimiter)
+    data = pd.read_csv(filepath, encoding='utf-8', sep=delimiter)
+    return header_row, data
+
+
+def read_xlsx(filepath):
+    '''
+    Read Given excel file.
+    '''
+    wb = xlrd.open_workbook(filepath)
+    sheet = wb.sheet_by_index(0)
+    header_row = sheet.row_values(0)
+    data = pd.read_excel(filepath, encoding='utf-8')
+    return header_row, data
+
+def missing_values_untyped(series, meta, max=0, values=['', 'NA']):
+    '''
+    Reports number of missing values in a series
+    '''
+    strings = series[series.apply(lambda v: isinstance(v, six.string_types))]
+    values = set(values)
+    na = strings.apply(lambda v: v in values).sum()
+    null = pd.isnull(series).sum()
+    missing = null + na
+    if missing > max:
+        return {
+            'code': 'missing_value_untyped',
+            'message': '{} | {:.0f}'.format(series.name, missing),
+            'series': series.name,
+            'missing': missing,
+            'na': na,
+            'null': null,
+        }
+
+
+def duplicate_rows_untyped(data, meta):
+    count_duplicates = data.duplicated().sum()
+    if count_duplicates > 0:
+        return {
+            'code': 'duplicate_rows_untyped',
+            'message': '{:.0f}'.format(count_duplicates),
+            'duplicates': count_duplicates,
+        }
+
+
+def check_numeric(series):
+    '''Given a string series, checks if the value is numeric or not.
+    - If fully numeric, no errors
+    - If it's supposed to be numeric (based on heuristics) but is not, report an error
+    - If it's clearly not numeric, no errors
+    '''
+    pass
+
+
+def count_numeric_outliers(series, low=None, high=None, max=0):
+    '''Given a numerical series, counts number of outliers
+    - If low is not specified, 2 percentile is taken
+    - If high is not specified, 98 percentile is taken
+
+    '''
+    if series._get_numeric_data().shape[0] == 0:
+        return None
+    q1 = series.quantile(0.25)
+    q3 = series.quantile(0.75)
+    iqr = q3 - q1
+    if low is None:
+        low = q1 - 1.5 * iqr
+    if high is None:
+        high = q3 + 1.5 * iqr
+    lower_outliers = series[series < low].shape[0]
+    upper_outliers = series[series > high].shape[0]
+    outliers = lower_outliers + upper_outliers
+    if outliers > max:
+        return {
+            'code': 'count_outliers_typed',
+            'message': 'Outliers count in {} column(numeric): \
+            {:.0f}'.format(series.name, outliers),
+            'series': series.name,
+            'outliers': outliers,
+            'lower_outliers': lower_outliers,
+            'upper_outliers': upper_outliers,
+            'low': low,
+            'high': high,
+        }
+
+
+def nulls_patterns(data):
+    '''
+    '''
+    nulls_pattern = {}
+    missing_cols = data.columns[pd.isnull(data).sum() > 0]
+    for comb in range(len(missing_cols), 0, -1):
+        for i in itertools.combinations(missing_cols, comb):
+            cols = list(i)
+            non_nulls = data[cols].dropna(how='all')
+            if(non_nulls.shape[0] < data.shape[0]):
+                nulls_pattern[i] = data.shape[0] - non_nulls.shape[0]
+                data = data.loc[non_nulls.index]
+    if len(nulls_pattern) > 1:
+        return {
+                'code': 'missing-patterns',
+                'message': 'missing patterns found',
+                'md_pattern': nulls_pattern
+        }
+
+
+def count_categorical_outliers(series):
+    # Need to handle long tail
+    if len(series._get_numeric_data()) == 1:
+        return None
+    series_freq = series.value_counts()
+    steepest_slope = series_freq[series_freq.diff() / series_freq.shift(1) < -0.5]
+    if len(steepest_slope):
+        outliers = len(series_freq[series_freq <= steepest_slope.values[0]])
+        message = 'Outliers count in %s column(categorical): %d' % (series.name, outliers)
+        return {
+            'code': 'count_categorical_outliers_typed',
+            'series': series.name,
+            'outliers': outliers,
+            'message': message
+        }
+
+
+def load(path_or_file, **kwargs):
+    '''
+    Returns a dictionary with 2 keys:
+
+    - ``data``: a Pandas DataFrame with the contents of the file. ``None`` if
+      the file could not be loaded
+    - ``error``: a list of file format or column types data errors
+    '''
+    pass
